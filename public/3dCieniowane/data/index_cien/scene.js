430ae3f30e966703373f7c3993d879fe44fd77aa
wzaprawa <wzaprawa@gmail.com>
2022-07-30 20:45:17 +0200

    Added shading to 3d vizualisation Added comparable 3d vizualisation Added comparable 2d maps

---
+++ public/3dCieniowane/data/index_cien/scene.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b9bda3d566be6215774828deb92047274a7494de2bcd9bb16d635c6eb354894
+size 134729711
---
+++ public/3dPodstawa/data/index/scene.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c32935887aad666e60e195312d895760d1a3eb0eb13c23b58f28a1d42094f5ed
+size 119949367
